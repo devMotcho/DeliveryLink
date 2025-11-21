8bc3264cacc6ec11500e891c2d9d554464bfdb05
devMotcho <code.mmd2@gmail.com>
2025-11-21 00:01:54 +0000

    Add initial templates and views for DeliveryLink app

--- web/delivery/views.py
+++ web/delivery/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    context = {
+        'title': 'Hello world'
+        }
     return render(request, 'delivery/index.html')
